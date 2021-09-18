f370ed7db9eec9892bb39c73ce9365e809016343
dylan <dylanhen@mit.edu>
2021-09-18 19:20:03 -0400

    wedges

--- c5torsor.py
+++ c5torsor.py
@@ -1,25 +1,46 @@
 from sethelper import *
 import math
+import random
 
 folder = 'c5torsor'
 
-colors = [(255,0,0), (0,255,0), (0,0,255)]
-diam = 280  # diameter of pentagons
-rad = 40 # radius of dots
+pentagons = [(255,0,0), (0,255,0), (0,0,255)]
+pent_rad = 140  # radius of pentagons
+dot_rad = 25 # radius of dots
 sep = 330 # distance between clocks
-thickness = 30 # of arms
-vertices = [(math.sin(math.pi*2/5*i)*diam/2, -math.cos(math.pi*2/5*i)*diam/2) for i in range(7)]
+thickness = 30 # of pentagon lines
+colors = [(255,153,0), (51,255,0), (0,255,255), (51,0,255), (255,0,153)]
+
+def drawpentagon(draw, cx, cy, inr, outr, theta, n, color):
+	# draws pentagon on draw with
+	# center cx,cy
+	# inner radius inr
+	# outner radius outr
+	# rotated by theta
+	# position n (0-4)
+	# vertices color
+	# draw.line([(cx+math.sin(theta+math.pi*2/5*i)*r, cy+math.cos(theta+math.pi*2/5*i)*r) for i in range(7)], c, 20, 'curve')
+	x = lambda i, r=outr: cx+math.sin(theta+math.pi*2/5*i)*r
+	y = lambda i, r=outr: cy+math.cos(theta+math.pi*2/5*i)*r
+
+	for i,c in enumerate(colors):
+		draw.line([x(i), y(i), x(i+1), y(i+1)], c, thickness)
+	draw.polygon([x(n+1,inr), y(n+1,inr), x(n,inr), y(n,inr), x(n), y(n), x(n+1), y(n+1)], colors[n])
+	for i in range(5):
+		draw.ellipse([x(i)-dot_rad, y(i)-dot_rad, x(i)+dot_rad, y(i)+dot_rad], color)
+	# hx, hy = math.sin(theta+math.pi*2/5*n)*r, math.cos(theta+math.pi*2/5*n)*r
+	# draw.line([cx+hx, cy+hy, cx, cy, cx+hx, cy+hy], (0,0,0), thickness, 'curve')
+	# draw.ellipse([cx+hx-dot_rad, cy+hy-dot_rad, cx+hx+dot_rad, cy+hy+dot_rad], c, (0,0,0), 4)
 
 
 
 for n in range(5**3):
 	img, draw = blankcard()
 	draw_orienter(draw)
-	for i,c in enumerate(colors):
-		draw.line([(cardwidth/2+x, cardheight/2+(i-1)*sep+y) for x,y in vertices], c, 20, 'curve')
-		hx, hy = vertices[(n//(5**i))%5]
-		draw.line([cardwidth/2+hx, cardheight/2+(i-1)*sep+hy, cardwidth/2, cardheight/2+(i-1)*sep, cardwidth/2+hx, cardheight/2+(i-1)*sep+hy], (0,0,0), thickness, 'curve')
-		draw.ellipse([cardwidth/2+hx-rad, cardheight/2+(i-1)*sep+hy-rad, cardwidth/2+hx+rad, cardheight/2+(i-1)*sep+hy+rad], c, (0,0,0), 4)
+	for i,c in enumerate(pentagons):
+		# drawpentagon(draw, cardwidth/2, cardheight/2+(i-1)*sep, 0, pent_rad, random.random()*2*math.pi, (n//(5**i))%5, c)
+		drawpentagon(draw, cardwidth/2, cardheight/2+(i-1)*sep, 0, pent_rad, 0, (n//(5**i))%5, c)
+		# drawpentagon(draw, cardwidth/2, cardheight/2, pent_rad*(2-i), pent_rad*(3-i), 0, (n//(5**i))%5, c)
 	img.save(f'{folder}/fronts/{n}.png')
 
 setback('C53T', (255, 0, 255)).save(f'{folder}/back.png')
