c9fe222d1d1060d0159ccc0f29654a247fc311b8
dylan <dylanhen@mit.edu>
2021-09-19 17:52:55 -0400

    added octahedra

--- wreathtorsor.py
+++ wreathtorsor.py
@@ -4,36 +4,66 @@ import math
 
 folder = 'wreathtorsor'
 
-hexrad = 400 # radius of outer hexagon
-thickness = 20 # black lines
-gap = 15 # white gaps
+hexrad = 200 # radius of outer hexagon
+thickness = 10 # black lines
+gap = 10 # white gaps
 dotrad = 100 # radius of white circles
-width = 70 # outlined rhombi
+width = 30 # outlined rhombi
+sep = 450 # between cube and octahedron
+parityrad = 60 # parity dot
 
-colors = [(255,0,0), (0,255,0), (0,0,255)]
+cubecolors = [(255,0,0), (0,255,0), (0,0,255)]
+octahedoncolors = [(155,0,255), (240, 150, 0), (255,0,200), (0,255,255)]
 
 x = lambda r, t: cardwidth/2+math.sin(t)*r
-y = lambda r, t: cardheight/2+math.cos(t)*r
-p = lambda r, t: (x(r,t), y(r,t))
+cy = lambda r, t: cardheight/2-sep/2+math.cos(t)*r
+cp = lambda r, t: (x(r,t), cy(r,t))
 
-for order in permutations(range(3)):
-	for dots in range(8):
+oy = lambda r, t: cardheight/2+sep/2+math.cos(t)*r
+op = lambda r, t: (x(r,t), oy(r,t))
+
+bit = lambda b, i: b>>i&1 
+# find octahedron color index
+colorvertex = lambda dots: 3*(dots>>2)^(dots&3)
+
+for order in permutations(range(3)): # order[j] is the position of color j
+	for dots in range(8): # bit(dots, j) says whether color j is outlined
 		img, draw = blankcard()
 		draw_orienter(draw)
-		for i,j in enumerate(order):
-			draw.polygon([p(0,0), p(hexrad, math.pi/3*(2*i-1)), p(hexrad, math.pi/3*(2*i)), p(hexrad, math.pi/3*(2*i+1))], colors[j])
-			if dots>>i&1:
+
+		# draw cube
+		for j,i in enumerate(order):
+			draw.polygon([cp(0,0), cp(hexrad, math.pi/3*(2*i-1)), cp(hexrad, math.pi/3*(2*i)), cp(hexrad, math.pi/3*(2*i+1))], cubecolors[j])
+			if bit(dots, j):
 				offsetx, offsety = math.sin(math.pi/3*2*i)*(width+thickness/2+gap), math.cos(math.pi/3*2*i)*(width+thickness/2+gap)
 				draw.polygon([
-					(x(0,0)+offsetx, y(0,0)+offsety),
-					(x(hexrad, math.pi/3*(2*i-1))+offsety*3**.5, y(hexrad, math.pi/3*(2*i-1))-offsetx*3**.5),
-					(x(hexrad, math.pi/3*(2*i))-offsetx, y(hexrad, math.pi/3*(2*i))-offsety),
-					(x(hexrad, math.pi/3*(2*i+1))-offsety*3**.5, y(hexrad, math.pi/3*(2*i+1))+offsetx*3**.5),
+					(x(0,0)+offsetx, cy(0,0)+offsety),
+					(x(hexrad, math.pi/3*(2*i-1))+offsety*3**.5, cy(hexrad, math.pi/3*(2*i-1))-offsetx*3**.5),
+					(x(hexrad, math.pi/3*(2*i))-offsetx, cy(hexrad, math.pi/3*(2*i))-offsety),
+					(x(hexrad, math.pi/3*(2*i+1))-offsety*3**.5, cy(hexrad, math.pi/3*(2*i+1))+offsetx*3**.5),
 					], (255,255,255))
+		# white gaps
 		for i in range(3):
-			draw.line([p(0,0), p(hexrad, math.pi/3*(2*i-1)), p(hexrad, math.pi/3*(2*i)), p(hexrad, math.pi/3*(2*i+1)), p(0,0)], (255,255,255), thickness+2*gap, 'curve')
+			draw.line([cp(0,0), cp(hexrad, math.pi/3*(2*i-1)), cp(hexrad, math.pi/3*(2*i)), cp(hexrad, math.pi/3*(2*i+1)), cp(0,0)], (255,255,255), thickness+2*gap, 'curve')
+		# black lines
 		for i in range(3):
-			draw.line([p(0,0), p(hexrad, math.pi/3*(2*i-1)), p(hexrad, math.pi/3*(2*i)), p(hexrad, math.pi/3*(2*i+1)), p(0,0)], (0,0,0), thickness, 'curve')
+			draw.line([cp(0,0), cp(hexrad, math.pi/3*(2*i-1)), cp(hexrad, math.pi/3*(2*i)), cp(hexrad, math.pi/3*(2*i+1)), cp(0,0)], (0,0,0), thickness, 'curve')
+		
+		# draw octahedron
+		draw.polygon([op(hexrad, math.pi/3*2*i) for i in range(3)], octahedoncolors[colorvertex(dots)])
+		for i in range(3):
+			draw.polygon([op(hexrad, math.pi/3*(2*i-1)+math.pi), op(hexrad, math.pi/3*(2*i)+math.pi), op(hexrad, math.pi/3*(2*i+1)+math.pi)], octahedoncolors[colorvertex(dots^(2**order.index(i)))])
+		# white gaps
+		draw.line([op(hexrad, math.pi/3*i) for i in range(8)], (255,255,255), thickness+2*gap, 'curve')
+		draw.line([op(hexrad, math.pi/3*2*i) for i in range(5)], (255,255,255), thickness+2*gap, 'curve')
+		# black lines
+		draw.line([op(hexrad, math.pi/3*i) for i in range(8)], (0,0,0), thickness, 'curve')
+		draw.line([op(hexrad, math.pi/3*2*i) for i in range(5)], (0,0,0), thickness, 'curve')
+
+		# parity dot
+		if bit(dots,0)^bit(dots,1)^bit(dots,2)^(sum(order[i]>order[j] for i in range(3) for j in range(i,3))%2):
+			draw.ellipse([x(0,0)-parityrad, oy(0,0)-parityrad, x(0,0)+parityrad, oy(0,0)+parityrad], (0,0,0))
+
 		img.save(f'{folder}/fronts/{order}{dots}.png')
 
 setback('CUBE', (50,200,50)).save(f'{folder}/back.png')
